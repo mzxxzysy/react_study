06c4d5aa6132bd49764c004d21d87342463dc9d3
mzxxzysy <jsyjj21@gmail.com>
2024-11-25 00:30:01 +0900

    한시간 만에 끝내는 React.js 입문

---
+++ 1hours/my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Counter from "./pages/Counter";
+import Input from "./pages/Input";
+import Input2 from "./pages/Input2";
+import UserList from "./pages/List";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link> | <Link to="/Input">Input</Link> |{" "}
+        <Link to="/Input2">Input2</Link> | <Link to="/list">List</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Counter" element={<Counter />} />
+        <Route path="/Input" element={<Input />} />
+        <Route path="/Input2" element={<Input2 />} />
+        <Route path="/List" element={<UserList />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
